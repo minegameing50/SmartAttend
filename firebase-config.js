901932fa51a9e1cf79bfafc9569ec518d68809e2
minegameing50 <atuldhanotiya7@gmail.com>
2026-03-26 20:46:11 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,6 @@
 // firebase-config.js
+export const auth
+export const db
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
