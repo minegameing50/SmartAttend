29b9035bc38cdf6f611a7ce889117a0395811ccd
minegameing50 <atuldhanotiya7@gmail.com>
2026-03-26 20:05:14 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,19 @@
-export const firebaseConfig = {
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
   apiKey:            "AIzaSyCvBqtqu9Cy-_yA3rxLCE_aR0kpvsWomSw",
   authDomain:        "student-attend-b28a8.firebaseapp.com",
   projectId:         "student-attend-b28a8",
-  databaseURL:       "https://student-attend-b28a8-default-rtdb.firebaseio.com",
   storageBucket:     "student-attend-b28a8.firebasestorage.app",
   messagingSenderId: "468349685878",
   appId:             "1:468349685878:web:819dd8406f37c062550f57"
 };
 
-export const SITE_URL = window.location.origin + window.location.pathname.replace(/\/[^/]*$/, '');
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
